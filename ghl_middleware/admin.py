f179a8455c42a617bb02881c3d98f8d908be09d7
creeyes <c.reeyes96@gmail.com>
2025-12-26 17:37:42 +0100

    Register Agencia, Propiedad, and Cliente models in admin

--- ghl_middleware/admin.py
+++ ghl_middleware/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Agencia, Propiedad, Cliente
 
-# Register your models here.
+# Esto hace que aparezcan en el panel y se vean bonitos con columnas
+@admin.register(Agencia)
+class AgenciaAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'location_id', 'api_key')
+    search_fields = ('nombre', 'location_id')
+
+@admin.register(Propiedad)
+class PropiedadAdmin(admin.ModelAdmin):
+    list_display = ('zona', 'precio', 'habitaciones', 'estado', 'agencia')
+    list_filter = ('estado', 'zona', 'agencia')
+    search_fields = ('zona', 'ghl_contact_id')
+
+@admin.register(Cliente)
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'presupuesto_maximo', 'zona_interes', 'agencia')
+    list_filter = ('zona_interes', 'agencia')
+    search_fields = ('nombre',)
