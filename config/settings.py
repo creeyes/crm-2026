cb263cb24df408e31ef8c65b6a8968626e011e64
Francisco T <frantrepa@gmail.com>
2026-01-10 18:55:35 +0100

    nueva version bbdd

--- config/settings.py
+++ config/settings.py
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # --- BASE DE DATOS (Auto-configurable) ---
 DATABASES = {
     'default': dj_database_url.config(
-        default='sqlite:///db.sqlite3',
+        default=os.environ.get('DATABASE_URL'),
         conn_max_age=600
     )
 }
