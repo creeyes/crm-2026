1ada04b34a6f024f4a3db4a3d0d4b06b2b2c55d0
FranTr64 <frantrepa@gmail.com>
2026-01-30 20:20:05 +0100

    mod 2

--- ghl_middleware/views.py
+++ ghl_middleware/views.py
@@ -6,6 +6,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from django.db.models import Q
+from django.http import JsonResponse
 
 from .models import Agencia, Propiedad, Cliente, GHLToken, Zona
 from .tasks import sync_associations_background
