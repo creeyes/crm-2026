d53c732319913ceb7e4eb48f6d2c6ea9bd6013a4
creeyes <c.reeyes96@gmail.com>
2026-02-03 18:34:15 +0100

    Change public properties endpoint URL
    Updated the public properties endpoint URL in urls.py.

--- GHL_Front/urls.py
+++ GHL_Front/urls.py
@@ -3,9 +3,11 @@ URL configuration for GHL_Front app.
 """
 from django.urls import path
 from . import views
+from .views import PublicPropertyList
 
 app_name = 'ghl_front'
 
 urlpatterns = [
-    path('api/public/properties/', PublicPropertyList.as_view(), name='public_properties'),
+    # Endpoint público: /api/front/properties/?agency_id=XXX
+    path('api/properties/', PublicPropertyList.as_view(), name='public_properties'),
 ]
